2175827c489229d862b487fdb9b7fa6cac0db391
Alexei Voyakin <aleksei_voyakin@mail.ru>
2019-10-02 15:43:27 +0300

    Initial program hw1 added

---
+++ project/include/simple.h
@@ -0,0 +1,6 @@
+#ifndef PROJECT_INCLUDE_SIMPLE_H
+#define PROJECT_INCLUDE_SIMPLE_H
+
+int simple_number_check(int number);
+
+#endif  //PROJECT_INCLUDE_SIMPLE_H
--- project/src/utils.c
+++ project/src/utils.c
@@ -1,14 +1,29 @@
 #include <string.h>
-
 #include "utils.h"
 
 size_t custom_strlen(const char* str) {
-    return sizeof(str);
+    return strlen(str);
 }
 
-// TODO: Implement `power of` function
-/*
-int custom_pow(int base, int power) {
-    return 0;
+int custom_pow(int base, int power)
+{
+    int minus = 0;
+    if (power == 0)
+        return 1;
+    else if (power == 1)
+        return base;
+    else if (power < 0)
+    {
+        minus++;
+        power *= -1;
+    }
+    float res = base;
+    while (power > 1)
+    {
+        res *= base;
+        power--;
+    }
+    if (minus == 1)
+        res = 1 / res;
+    return (int) res;
 }
-*/
