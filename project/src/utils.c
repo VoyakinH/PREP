4a66b225129fcaf42af7cad6b822b14bcca054fd
Alexei Voyakin <aleksei_voyakin@mail.ru>
2019-10-04 08:56:03 +0300

    Style corrected hw1

--- project/src/utils.c
+++ project/src/utils.c
@@ -9,16 +9,13 @@ int custom_pow(int base, int power) {
     if (power == 0) {
         return 1;
     }
-    
     float res = base;
     float base_f = base;
-    
     if (power < 0) {
         res = 1 / base;
         power *= -1;
         base_f = 1 / base_f;
     }
-    
     while (power > 1) {
         res *= base_f;
         power--;
